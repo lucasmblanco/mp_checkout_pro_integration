c92be431ee99dab04468547e115369a2a1e2d72a
Lucas <lucasmatiasblanco@outlook.com>
2023-11-02 22:45:53 -0300

    Fixes in payments route

--- app/api/payments/route.ts
+++ app/api/payments/route.ts
@@ -1,15 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { MercadoPagoConfig, Payments } from 'mercadopago';
+import { MercadoPagoConfig, Payment } from 'mercadopago';
+import { PaymentSearchData } from 'mercadopago/dist/clients/payment/search/types';
 
-const client = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN });
-
+const client = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN ?? '' });
 
 export async function GET() {
-    const payments = new Payments(client);
-    const params = {
-        criteria: 'desc',
+    const payments = new Payment(client);
+    const params: PaymentSearchData = {
+        options: {
+            criteria: 'desc',
           sort: 'date_created',
           external_reference: 'lucasmatiasblanco@outlook.com'
+            }
         }
     try {
         const result = await payments.search(params); 
